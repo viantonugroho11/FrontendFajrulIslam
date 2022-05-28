275ba13fe90896dcede9c4ee584877203c3e1966
Muhammad Novianto Nugroho <viantonugroho@student.gunadarma.ac.id>
2022-05-28 11:17:31 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        base: "#2F313D",
+        hover:"#81A084"
+      },
+    },
+  },
+  plugins: [],
+};
