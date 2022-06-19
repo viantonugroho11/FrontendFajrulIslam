d8afa7c76039648c119eebb192e7c6fb08a693b2
hanifMutaqin <mhnfmtqn@gmail.com>
2022-06-19 19:43:35 +0700

    [hanif] init vite-tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,31 @@
 // const colors = require('tailwindcss/colors')
 
 module.exports = {
-  content: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
+  content: ["index.html", "./src/**/*.{js,jsx,ts,tsx,vue,html}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        base: "#2F313D",
+        hover: "#81A084",
+      },
+      fontSize: {
+        "2vw": [
+          "2vw",
+          {
+            lineHeight: "1",
+          },
+        ],
+        "7vw": [
+          "7vw",
+          {
+            lineHeight: "1.2",
+          },
+        ],
+      },
+      maxWidth: {
+        "8xl": "1366px",
+      },
+    },
   },
   plugins: [],
-}
+};
